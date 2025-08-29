1d50fb6f2bd7941cc2a02d5b4f049d43f4758e5f
Jonathan <jonathan@forestandrock.co.uk>
2025-08-29 22:10:30 +0200

    Typed Delete function

--- src/app/api/classes/[id]/route.ts
+++ src/app/api/classes/[id]/route.ts
@@ -2,9 +2,15 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
+
+type RouteContext = {
+  params: { id: string };
+};
+
+
 const prisma = new PrismaClient();
 
-export async function DELETE(req: Request, { params }: object) {
+export async function DELETE(req: Request, { params }: RouteContext) {
   const id = parseInt(params.id, 10);
   await prisma.class.delete({ where: { id } });
   return NextResponse.json({ success: true });
