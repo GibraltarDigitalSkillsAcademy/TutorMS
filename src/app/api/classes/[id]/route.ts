436f4661b8bc90552bf215dbea79281e1b0aa5fc
Jonathan <jonathan@forestandrock.co.uk>
2025-08-29 22:06:10 +0200

    Fixed DELETE issue

--- src/app/api/classes/[id]/route.ts
+++ src/app/api/classes/[id]/route.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function DELETE(_: Request, { params }: { params: { id: string } }) {
+export async function DELETE(req: Request, { params }: object) {
   const id = parseInt(params.id, 10);
   await prisma.class.delete({ where: { id } });
   return NextResponse.json({ success: true });
